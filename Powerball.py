80a987e11616f08ea3a340749284066709b4f432
Tremule <latremom@gmail.com>
2015-04-15 00:10:42 -0400

    Create Powerball.py

---
+++ Powerball.py
@@ -0,0 +1,26 @@
+from random import randint
+
+def number_generator():
+	draw = []
+	while len(draw) < 5:
+		ballvall = randint(1,53)
+		if ballvall in draw:
+			pass
+		else : 
+			draw.append(ballvall)
+	powerball = (randint(1,42))
+	return sorted(draw), powerball	
+
+def draw_number(sets):
+	for i in range(sets):
+		draw = number_generator()
+		print('Your numbers: {:<2}    \tPowerball: {}'.format(str(draw[0]).strip('[]'),draw[1]))
+
+
+if __name__ == "__main__":
+	print('Official (but fruitless) Powerball number generator') 
+	try:
+		number_of_draws = int(input('How many sets of numbers? >> '))
+		draw_number(number_of_draws)
+	except ValueError:
+		print("That isn't a number, please try again")
