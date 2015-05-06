d4dba9a17e5d0538c35448a08561289e674ddc1a
tremule <latremom@gmail.com>
2015-05-06 18:32:47 -0400

    added bootstrap

--- forms.py
+++ forms.py
@@ -3,7 +3,7 @@ from flask_wtf import Form
 from wtforms import IntegerField, validators, DecimalField
 
 class LottoSetup(Form):
-	number_of_draws = IntegerField('How Many Draws:',[validators.NumberRange(min=1, max=10)])
+	number_of_draws = IntegerField('How Many Draws:',[validators.NumberRange(min=1, max=99)])
 	cost_of_draw = DecimalField('Cost of a Draw: ', places =2, rounding=None)
 
 
--- run.py
+++ run.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
 from powerball import number_generator
 from forms import LottoSetup
 
 app = Flask(__name__)
 app.config.from_object('config.DevConfig')
+Bootstrap(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = LottoSetup()
     if request.method =='POST':
         if request.form['submit'] == 'draw' and form.validate_on_submit():
+            #for i in range(form.number_of_draws):
             number, power = number_generator()
             return render_template('draw.html',
                 number=number, power=power, num_draw=form.number_of_draws.data)
 
+        if request.form['submit'] == 'return':
+            return render_template('draw_setup.html', form=form)
+
         else:
-            flash('Something went wrong, try again')
+            flash('Type an number between 1 and 99')
             return render_template('draw_setup.html', form=form)
 
     return render_template('draw_setup.html', form=form)
